cc66b36a115825bca781df71d276bf8ac6253ddf
Chaitu1712 <chaitanyapandey62@gmail.com>
2025-04-07 03:18:03 +0530

    refactor: Remove unnecessary configuration from next.config.mjs

--- frontend/code-translator-frontend/next.config.mjs
+++ frontend/code-translator-frontend/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig ={
-    reactStrictMode: true,
-    devIndicators: {
-      buildActivity: false, // Disable build activity indicator
-      buildActivityPosition: 'bottom-right', // (Optional) Change position
-    },
-  };
+const nextConfig ={};
 
 export default nextConfig;
