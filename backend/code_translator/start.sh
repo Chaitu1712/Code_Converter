1ed16e904c35da80d1a74bf53004d04a66ab824c
Chaitu1712 <chaitanyapandey62@gmail.com>
2025-04-07 04:02:52 +0530

    feat: Update start.sh and Dockerfile for deployment on render

--- backend/code_translator/start.sh
+++ backend/code_translator/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Start Flask in the background
-python backend/code_translator/src/main/python/translate_microservice/app.py &
+python app/code_translator/src/main/python/translate_microservice/app.py &
+sleep 10
 # Start Java application
-java -jar backend/code_translator/target/code_translator-0.0.1-SNAPSHOT.jar
+cd app/code_translator
+./mvnw clean package
+java -jar app/code_translator/target/code_translator-0.0.1-SNAPSHOT.jar
